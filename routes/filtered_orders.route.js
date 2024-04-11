81c9b79e92afe01401379f5ebb4ba2cafcc7545f
JAS-30 <163284452+JAS-30@users.noreply.github.com>
2024-04-11 14:54:36 +0200

    Update filtered_orders.route.js

--- routes/filtered_orders.route.js
+++ routes/filtered_orders.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-const filtered_route = express.Router();
 import{
     filteredOrders,filteredOrdersByID
 }from '../controller/filtered_orders.controller.js';
+
+const filtered_route = express.Router();
+
 
 filtered_route.get("/", filteredOrders);
 filtered_route.get("/:id", filteredOrdersByID);
